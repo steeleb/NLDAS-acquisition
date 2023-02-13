6b08a92602236360de22310c892d0ebddce313e1
Bethel Steele <steeleb@IES.local>
2023-02-12 21:28:37 -0700

    with config, troubleshooting

---
+++ combineNLDAS_config.R
@@ -0,0 +1,64 @@
+###########################################################
+### Downloading NLDAS2 data for meteorological hourly forcing
+### http://ldas.gsfc.nasa.gov/nldas/NLDAS2forcing.php
+### Author: Hilary Dugan hilarydugan@gmail.com
+### Date: 2019-09-30
+###########################################################
+
+#v09Mar21 BGS: removed reliance on other script objects; nc_open timing out after about 3 months of data, so added an additional loop in the read function to open by month and added nc_close to script.
+#v05Mar21 BGS: removed hardcoding after line 35
+
+library(lubridate)
+library(ncdf4)
+library(tidyverse)
+
+
+###########################################################
+### Set up the output data frame
+###########################################################
+
+#save the variable names in nc files
+vars_nc = c('TMP','SPFH', 'PRES', 'UGRD', 'VGRD', 'DLWRF', 'CONVfrac', 'CAPE', 'PEVAP', 'APCP', 'DSWRF')
+
+#set up output dataframe for the number of cells above and the number of columns of data
+output <- NULL
+for (l in 1:length(vars_nc)){
+  colClasses = c("POSIXct", rep("numeric",cellNum))
+  col.names = c('dateTime',rep(vars_nc[l],cellNum))
+  output[[l]] = read.table(text = "",colClasses = colClasses,col.names = col.names)
+  attributes(output[[l]]$dateTime)$tzone = 'GMT'
+}
+
+###########################################################
+### Run file list loop
+###########################################################
+# Start the clock!
+ptm <- proc.time()
+
+nc_files <- list.files(dumpdir_nc)
+
+for (i in 1:length(nc_files)) {
+  print(nc_files[i])
+    
+  for (v in 1:length(vars_nc)) {
+    nldasvar <- vars_nc[v]
+    br = nc_open(paste0(dumpdir_nc,nc_files[i]))
+    output[[v]][i,1] =  as.POSIXct(paste0(substr(nc_files[i], 1, 4),'-', substr(nc_files[i], 5,6), '-', substr(nc_files[i], 7,8), ' ', substr(nc_files[i], 9,10), ':', substr(nc_files[i], 11,12)), tz=loc_tz)
+    output[[v]][i,-1] = ncvar_get(br, nldasvar)
+    nc_close(br)
+  }
+  rm(br)
+}
+
+# Stop the clock
+proc.time() - ptm
+
+###########################################################
+### save each variable in a .csv
+###########################################################
+for (f in 1:length(vars_nc)){
+  write.csv(output[[f]],paste0(dumpdir_csv, lake_name, vars_nc[f],'.csv'),row.names=F)
+}
+
+
+
