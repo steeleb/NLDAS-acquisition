76d31d34e7f801fc9b0c63fcf74b4d18bd4fa34f
B. Steele <steeleb@caryinstitute.org>
2023-02-13 21:11:40 -0700

    update get and combine to use yml config file

--- combineNLDAS_config.R
+++ combineNLDAS_config.R
@@ -2,13 +2,12 @@
 ### Downloading NLDAS2 data for meteorological hourly forcing
 ### http://ldas.gsfc.nasa.gov/nldas/NLDAS2forcing.php
 ### Author: Hilary Dugan hilarydugan@gmail.com
+### Author: B Steele steeleb@caryinstitute.org
 ### Date: 2019-09-30
+### Last Updated: 2023-02-13
 ###########################################################
 
-#v09Mar21 BGS: removed reliance on other script objects; nc_open timing out after about 3 months of data, so added an additional loop in the read function to open by month and added nc_close to script.
-#v05Mar21 BGS: removed hardcoding after line 35
-
-library(lubridate)
+# library(lubridate)
 library(ncdf4)
 library(tidyverse)
 
@@ -18,12 +17,16 @@ library(tidyverse)
 ###########################################################
 
 #save the variable names in nc files
-vars_nc = c('TMP','SPFH', 'PRES', 'UGRD', 'VGRD', 'DLWRF', 'CONVfrac', 'CAPE', 'PEVAP', 'APCP', 'DSWRF')
+vars_nc = setup$params
+
+test = nc_open(file.path(setup$dumpdir_nc, list.files(setup$dumpdir_nc)[1]))
+cellNum = test$dim$time$len
+nc_close(test)
 
 #set up output dataframe for the number of cells above and the number of columns of data
 output <- NULL
 for (l in 1:length(vars_nc)){
-  colClasses = c("POSIXct", rep("numeric",cellNum))
+  colClasses = c("POSIXct", rep("numeric",cellNum)) 
   col.names = c('dateTime',rep(vars_nc[l],cellNum))
   output[[l]] = read.table(text = "",colClasses = colClasses,col.names = col.names)
   attributes(output[[l]]$dateTime)$tzone = 'GMT'
@@ -35,15 +38,15 @@ for (l in 1:length(vars_nc)){
 # Start the clock!
 ptm <- proc.time()
 
-nc_files <- list.files(dumpdir_nc)
+nc_files <- list.files(setup$dumpdir_nc)
 
 for (i in 1:length(nc_files)) {
   print(nc_files[i])
     
   for (v in 1:length(vars_nc)) {
     nldasvar <- vars_nc[v]
-    br = nc_open(paste0(dumpdir_nc,nc_files[i]))
-    output[[v]][i,1] =  as.POSIXct(paste0(substr(nc_files[i], 1, 4),'-', substr(nc_files[i], 5,6), '-', substr(nc_files[i], 7,8), ' ', substr(nc_files[i], 9,10), ':', substr(nc_files[i], 11,12)), tz=loc_tz)
+    br = nc_open(paste0(setup$dumpdir_nc,nc_files[i]))
+    output[[v]][i, 1] = as.POSIXct(paste0(substr(nc_files[i], 1, 4),'-', substr(nc_files[i], 5,6), '-', substr(nc_files[i], 7,8), ' ', substr(nc_files[i], 9,10), ':', substr(nc_files[i], 11,12)), tz='UTC')
     output[[v]][i,-1] = ncvar_get(br, nldasvar)
     nc_close(br)
   }
@@ -57,7 +60,7 @@ proc.time() - ptm
 ### save each variable in a .csv
 ###########################################################
 for (f in 1:length(vars_nc)){
-  write.csv(output[[f]],paste0(dumpdir_csv, lake_name, vars_nc[f],'.csv'),row.names=F)
+  write.csv(output[[f]],paste0(setup$dumpdir_csv, setup$lake_name, vars_nc[f],'.csv'),row.names=F)
 }
 
 
---
+++ getNLDAS_config.R
@@ -0,0 +1,115 @@
+###########################################################
+### Downloading NLDAS2 data for meteorological hourly forcing
+### http://ldas.gsfc.nasa.gov/nldas/NLDAS2forcing.php
+### Author: Hilary Dugan hilarydugan@gmail.com
+### Author: B Steele steeleb@caryinstitute.org
+### Date: 2019-09-30
+### Last Updated: 2023-02-13
+###########################################################
+
+# library(RCurl)
+library(lubridate)
+library(raster)
+# library(ncdf4)
+library(sf)
+# library(httr)
+# library(curl)
+library(stringr)
+library(yaml)
+
+###########################################################
+### Load YAML files
+###########################################################
+setup = read_yaml('setup.yml')
+secrets = read_yaml('secrets.yml')
+
+###########################################################
+### Create dump directories if they do not exist
+###########################################################
+if(!dir.exists(setup$dumpdir_nc)) {dir.create(setup$dumpdir_nc)}
+if(!dir.exists(setup$dumpdir_csv)) {dir.create(setup$dumpdir_csv)}
+
+###########################################################
+### Load password information
+###########################################################
+username = secrets$username
+password = secrets$password
+
+###########################################################
+### Set bounding extent
+###########################################################
+if(length(setup$extent) != 4) {
+  shape = st_read(setup$shapefile)
+  shape_wgs = st_transform(shape, crs = 'EPSG:4326')
+  extent = st_bbox(shape_wgs)
+} else {extent = setup$extent}
+
+###########################################################
+### Set timeframe from config
+###########################################################
+startdatetime = setup$startdatetime
+enddatetime = setup$enddatetime
+loc_tz = setup$loc_tz
+
+# sequence the datetime over your desired time period
+out.ts = seq.POSIXt(with_tz(as.POSIXct(startdatetime, tz = loc_tz), 'UTC'),
+                    with_tz(as.POSIXct(enddatetime,tz=loc_tz), 'UTC'), 
+                    by = 'hour')
+
+# Create output list of tables
+output = list()
+
+###########################################################
+### Run hourly loop
+###########################################################
+# Start the clock!
+ptm <- proc.time()
+
+for (i in 1:length(out.ts)) {
+  print(out.ts[i])
+  yearOut = year(out.ts[i])
+  monthOut = format(out.ts[i], "%m")
+  dayOut = format(out.ts[i], "%d")
+  hourOut = format(out.ts[i], "%H%M")
+  doyOut = format(out.ts[i],'%j')
+
+  filename = format(out.ts[i], "%Y%m%d%H%M")
+
+  ## Patricia Tran note (2020-12-03 : I updated the link the webpage)
+
+  URL <- paste0('https://hydro1.gesdisc.eosdis.nasa.gov/daac-bin/OTF/HTTP_services.cgi?FILENAME=%2Fdata%2FNLDAS%2FNLDAS_FORA0125_H.002%2F',
+               yearOut, '%2F',
+               str_pad(as.numeric(yday(as.Date(paste0(yearOut,"-", monthOut,'-' ,dayOut)))), 3, pad = "0"), ## The URL changes for every chunk of 24 hours
+               '%2FNLDAS_FORA0125_H.A',
+               yearOut, monthOut, dayOut, '.',
+               hourOut,  '.002.grb&FORMAT=bmM0Lw&BBOX=', 
+               round(as.numeric(extent[2]), 2),'%2C', # In the new version of the URL, the coordinates are only up to 2 digits
+               round(as.numeric(extent[1]), 2),'%2C',
+               round(as.numeric(extent[4]), 2),'%2C',
+               round(as.numeric(extent[3]), 2),
+               '&LABEL=NLDAS_FORA0125_H.A',
+               yearOut,monthOut,dayOut,'.',
+               hourOut,
+               '.002.grb.SUB.nc4&SHORTNAME=NLDAS_FORA0125_H&SERVICE=L34RS_LDAS&VERSION=1.02&DATASET_VERSION=002'
+               )
+  
+
+  lk <- URL
+  
+  h <- curl::new_handle()
+  
+  curl::handle_setopt(
+    handle = h,
+    httpauth = 1,
+    userpwd = paste0(username, ':', password)
+  )
+
+  resp <- curl::curl_fetch_disk(url = lk, 
+                                path = file.path(setup$dumpdir_nc,paste0(filename,'.nc')), 
+                                handle = h)
+
+  Sys.sleep(2)
+  
+}
+# Stop the clock
+proc.time() - ptm
